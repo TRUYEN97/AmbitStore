3ed2c72dd61c76bccc2d6e135971de90af1663dd
SQT <samtruyen60@mail.com>
2022-09-15 14:05:13 +0700

    tiep

--- src/Control/Client/ClientReceiver.java
+++ src/Control/Client/ClientReceiver.java
@@ -7,6 +7,7 @@ package Control.Client;
 import Communicate.Cmd.Cmd;
 import FileTool.FileService;
 import Model.PcInformation;
+import Model.Servants;
 import Unicast.commons.Actions.FileTransfer;
 import Unicast.commons.Actions.Object.List.ListPackage;
 import Unicast.commons.Actions.Object.MyName;
@@ -15,6 +16,7 @@ import Unicast.commons.Actions.SimplePackage;
 import Unicast.commons.Interface.ISend;
 import Unicast.commons.Interface.IObjectReceiver;
 import View.UI;
+import java.util.Map;
 
 /**
  *
@@ -25,10 +27,12 @@ public class ClientReceiver implements IObjectReceiver<SimplePackage> {
     private ISend<SimplePackage> handler;
     private final UI display;
     private final FileService fileService;
+    private final Servants servants;
 
     public ClientReceiver(UI display) {
         this.display = display;
         this.fileService = new FileService();
+        this.servants = new Servants();
     }
 
     @Override
@@ -38,29 +42,35 @@ public class ClientReceiver implements IObjectReceiver<SimplePackage> {
                 PcInformation pcInfo = PcInformation.getInstance();
                 this.handler.send(new MyName(
                         pcInfo.getComputerName(),
-                           pcInfo.getSystemName(),
-                          pcInfo.getLine(),
-                          pcInfo.getAllHardwareAddress()));
+                        pcInfo.getSystemName(),
+                        pcInfo.getLine(),
+                        pcInfo.getAllHardwareAddress()));
             }
+            
             case RUN -> {
-                ObjectPackage<String> list = (ObjectPackage<String>) pkg;
-                new Cmd().sendCommand(list.getdata());
+                ObjectPackage<String> command = (ObjectPackage<String>) pkg;
+                servants.runCmd(command.getdata());
             }
+            
+            case GOOD_BYE -> {
+                System.exit(0);
+            }
+            
             case FILE_TRANSFER -> {
                 FileTransfer fileTransfer = (FileTransfer) pkg;
                 byte[] data = fileTransfer.getData();
                 String path = fileTransfer.getFilePath();
-                if (path == null) {
+                long length = fileTransfer.getLength();
+                if (path == null || length != data.length) {
                     return;
                 }
                 this.fileService.saveFile(path, data);
             }
             case DOWN_LOAD -> {
-                ListPackage<String> fileTransfer = (ListPackage<String>) pkg;
-                
+                ObjectPackage<Map<String, String>> fileTransfer = (ObjectPackage<Map<String, String>>) pkg;
+                servants.ftpDownload(fileTransfer.getdata());
             }
-            default ->
-                throw new AssertionError();
+
         }
     }
 
---
+++ src/Model/Servants.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+import Communicate.Cmd.Cmd;
+import Communicate.FtpClient.FtpClient;
+import java.util.Map;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Servants {
+
+    private final FtpClient ftpClient;
+    private final Cmd cmd;
+    private String ftpHost;
+
+    public Servants() {
+        this.ftpClient = new FtpClient();
+        this.cmd = new Cmd();
+    }
+
+    public boolean runCmd(String command) {
+        return cmd.sendCommand(command);
+    }
+
+    public void ftpDownload(Map<String, String> paths) {
+        for (String key : paths.keySet()) {
+            ftpDownload(key, paths.get(key));
+        }
+    }
+    
+    public void ftpDownload(String ftpPath, String localFile) {
+        ftpClient.connect(ftpPath, 0);
+        ftpClient.downloadFile(ftpPath, localFile);
+    }
+}
